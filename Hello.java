9b4627bc7ca9027ba943acc40abe51e3921b9b10
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2021-02-15 18:13:06 +0900

    Add files via upload
    Hello Java , 안녕하세요 출력

---
+++ Hello.java
@@ -0,0 +1,8 @@
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		System.out.print("Hello Java\n"); // \n = 줄바꿈 {} = 범위(class를 벗어날 수 없음)
+		System.out.print("안녕하세요~"); // ; = 마침표 자바는 항상 써야함.
+	}
+}

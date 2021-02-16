e6b420207811e75af90637c5fbf62674e6a62ecd
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2021-02-16 18:00:50 +0900

    IdentifierEx

---
+++ IdentifierEx.java
@@ -0,0 +1,26 @@
+
+public class IdentifierEx {
+	
+	public static void main(String[] args) {
+		
+		int age = 20;
+		int Age = 21;
+		int aGe = 22;
+		
+		System.out.println(age);
+		System.out.println(Age);
+		System.out.println(aGe);
+		
+//		int phone Number = 10;
+		// 주석 단축키 ctrl + / 
+		int phoneNumber = 4;
+		
+		int num1 = 1;
+		int num2 = 2;
+		
+		// 키워드는 이름으로 지을 수 없다.
+//		int class = 10;
+//		int public = 11;
+		
+	}
+}

b289c8a91936eb739fbcddc6b892050d80c3dcdf
chocolate90 <74293804+chocolate90@users.noreply.github.com>
2021-02-24 18:31:28 +0900

    arrayInsert 예제

---
+++ ArrayInsert.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayInsert {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		String[] arr = new String[100];
+		
+		int index = 0; // 입력받은 개수(or 배열의 위치)를 나타낼 변수
+		
+		System.out.println("# 먹고 싶은 음식을 입력하세요.");
+		System.out.println("# 중지하려면 [그만]을 입력하세요.");
+		while(true) {
+			System.out.print(">");
+			String menu = scan.next();
+			
+			if(menu.equals("그만")) break;
+			
+			arr[index] = menu;
+			index++;
+			
+		}
+		
+		// 출력문
+		System.out.print("먹고싶은 음식:[");
+		for(int i = 0; i < index ; i++) {
+			System.out.print(arr[i]);
+			if(i == index-1) break;
+			System.out.print(", ");
+		}
+		System.out.print("]");
+	}
+}
